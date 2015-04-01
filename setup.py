b91afba63022523a7aa44d7b3d72dc865433229b
Ben Hamill <git-commits@benhamill.com>
2015-04-01 16:37:50 -0500

    Prepare for MMF tools.

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
+# encoding: utf-8
+
 from distutils.core import setup
+import sys
 
 import linguo
 
+version = linguo.__version__
+
+if 'sdist' in sys.argv:
+    import mmf_release_tools
+    version = mmf_release_tools.generate_release_version(version, __file__)
+    mmf_release_tools.write_release_version(version)
+else:
+    with open("RELEASE-VERSION", "r") as f:
+        version = f.readlines()[0].strip()
 
 setup(
     name='django-linguo',
     packages=['linguo', 'linguo.tests'],
     package_data={'linguo': ['tests/locale/*/LC_MESSAGES/*']},
-    version=linguo.__version__,
+    version=version,
     description=linguo.__doc__,
     long_description=open('README.md').read(),
     classifiers=[
